cb922de84a372225b5e9891c1422a41a01d73879
Aldo Mendoza <aldomendoza56@gmail.com>
2024-02-05 11:31:08 -0800

    working on routes

--- controllers/api/hikingRoutes.js
+++ controllers/api/hikingRoutes.js
@@ -1,17 +1,37 @@
 const router = require('express').Router();
-const { Model } = require('../../models');
+const { Hike } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req,res) => {
     try {
-        const newHiking = await Hiking.create({
+        const newHike = await Hike.create({
             ...req.body,
             user_id: req.session.user._id,
         });
-        res.status(200).json(newHiking)
+        res.status(200).json(newHike)
     } catch (err) {
         res.status(400).json(err);
     }
 });
 
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const hikeData = await Hike.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+        if (!hikeData) {
+            res.status(404).json({ message: "no hike found with this id!"});
+            return;
+        }
+
+        res.status(200).json(hikeData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    });
+
+module.exports = router;
 
