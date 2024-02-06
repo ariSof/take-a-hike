b670484b3fcecd47d799ac9bd402f1682bd7d521
Jay Luna <Lunajt@gmail.com>
2024-02-06 10:29:07 -0800

    wireframe for frontend

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,13 +2,22 @@ const router = require('express').Router();
 // const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get("/",(req,res)=>{
-    res.render('home')
-})
+// this is where you add the routes for each of your pages
+router.get("/",(req, res)=>{
+    res.render('home');
+});
 
-router.get("/abouthike",(req,res)=>{
-    res.render('about')
-})
+router.get("/home",(req, res)=>{
+    res.render('home');
+});
+
+router.get("/abouthike",(req, res)=>{
+    res.render('about');
+});
+
+router.get("/review",(req, res)=>{
+    res.render('review');
+});
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
@@ -16,7 +25,6 @@ router.get('/login', (req, res) => {
     res.redirect('/profile');
     return;
   }
-
   res.render('login');
 });
 
