2098bc2ca04272443de1bc5995a5a69014ab6c1c
Aldo Mendoza <aldomendoza56@gmail.com>
2024-02-07 10:11:20 -0800

    fix post route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -16,18 +16,6 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.post('/', async (req, res) => {
-  try {
-    const userData = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    res.status(200).json(userData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
 
 
 router.post('/login', async (req, res) => {
