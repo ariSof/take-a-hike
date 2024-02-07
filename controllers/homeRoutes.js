49f4acfec77add0ac35f4e8ce8d219d41a3408e3
Aldo Mendoza <aldomendoza56@gmail.com>
2024-02-06 18:49:47 -0800

    added additonal routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -36,6 +36,31 @@ router.get("/review",(req, res)=>{
     res.render('review');
 });
 
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        
+    const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Hike }],
+    })
+
+    const user = userData.get({ plain:true });
+
+    res.render('profile', {
+        ...user,
+        logged_in: true
+    });
+} catch (err) {
+    res.status(500).json(err);
+}
+});
+
+
+
+
+
+
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
