ab46b754b2210aca530fafbea38dc1edf39aae69
prieta <sofiaboubion@gmail.com>
2024-02-07 11:32:11 -0800

    added search route to look for difficutly parameter

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 
 
-const apiRoutes = require('./api');
+//const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const hikingRoutes = require('./api/hikingRoutes');
 
 router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+//router.use('/api', apiRoutes);
+router.use('/api', hikingRoutes);
 
 
 module.exports = router;
