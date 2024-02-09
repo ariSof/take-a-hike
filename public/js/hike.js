74d131bfdc54d47d20bccc305a0f2fc1f8b9ecee
prieta <sofiaboubion@gmail.com>
2024-02-09 13:16:40 -0800

    quick fix

--- public/js/hike.js
+++ public/js/hike.js
@@ -34,8 +34,7 @@ function displayHikeCards(hikeList) {
         buttonEl.classList.add('btn');
         buttonEl.classList.add('btn-primary');
         
-        //TODO: change link to heroku endpoint
-        buttonEl.href = "http://localhost:3001/review?id=" + hike.id;
+        buttonEl.href = "/hike/" + hike.id;
         buttonEl.textContent = 'Check it out!';
 
         //build my card element
